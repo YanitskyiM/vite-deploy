bd44eee151e201df27a43af51527f51ba3a86f65
Mykhailo Yanitskyi <mykhailoyanitskyi@nerdzlab.com>
2023-12-13 17:58:35 +0200

    add strorybook fix2

---
+++ storybook-static/assets/Input.stories-pj8fPt-g.js
@@ -0,0 +1,9 @@
+import{j as s}from"./jsx-runtime-AgcCsxC8.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";function a({type:e}){return s.jsxs("div",{className:"relative",children:[s.jsx("label",{htmlFor:"name",className:"absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900",children:e==="password"?"Password":"Name"}),s.jsx("input",{type:e,id:"name",className:"block w-40 rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6",placeholder:e==="password"?"":"Jane Smith"})]})}try{a.displayName="Input",a.__docgenInfo={description:"",displayName:"Input",props:{type:{defaultValue:null,description:"",name:"type",required:!0,type:{name:"enum",value:[{value:'"text"'},{value:'"password"'}]}}}}}catch{}const g={title:"Example/Input",component:a},r={args:{type:"text"}},t={args:{type:"password"}};var o,n,p;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    type: 'text'
+  }
+}`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};var d,i,c;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    type: 'password'
+  }
+}`,...(c=(i=t.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};const x=["InputText","InputPassword"];export{t as InputPassword,r as InputText,x as __namedExportsOrder,g as default};
